e1320964ec28a0d31722910f515e7902f3662dcf
Eldar Babić <ebabic1@etf.unsa.com>
2026-03-25 01:37:00 +0100

    Enable Flyway migrations, add Swagger configuration, and rename comment column in reviews table

---
+++ src/main/java/ba/unsa/etf/nbp/travel/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.nbp.travel.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("NBP Travel Management API")
+                        .version("1.0")
+                        .description("Travel Management System - Advanced Databases Course"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Paste your JWT token here (without 'Bearer ' prefix)")));
+    }
+}
